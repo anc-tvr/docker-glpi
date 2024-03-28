780a2a1a4c2f73261bd35fb9a503c4942d2b12c5
ANC <135969964+anc-tvr@users.noreply.github.com>
2024-03-28 12:19:48 +0300

    Update glpi-start.sh

--- glpi-start.sh
+++ glpi-start.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-#Controle du choix de version ou prise de la latest
+# Контроль выбора версии или взятие последней
 [[ ! "$VERSION_GLPI" ]] \
 	&& VERSION_GLPI=$(curl -s https://api.github.com/repos/glpi-project/glpi/releases/latest | grep tag_name | cut -d '"' -f 4)
 
-if [[ -z "${TIMEZONE}" ]]; then echo "TIMEZONE is unset"; 
+if [[ -z "${TIMEZONE}" ]]; then echo "Часовой пояс не установлен."; 
 else 
 echo "date.timezone = \"$TIMEZONE\"" > /etc/php/8.3/apache2/conf.d/timezone.ini;
 echo "date.timezone = \"$TIMEZONE\"" > /etc/php/8.3/cli/conf.d/timezone.ini;
 fi
 
-#Enable session.cookie_httponly
+# Включение session.cookie_httponly
 sed -i 's,session.cookie_httponly = *\(on\|off\|true\|false\|0\|1\)\?,session.cookie_httponly = on,gi' /etc/php/8.3/apache2/php.ini
 
 FOLDER_GLPI=glpi/
 FOLDER_WEB=/var/www/html/
 
-#check if TLS_REQCERT is present
+# Проверка, присутствует ли TLS_REQCERT
 if !(grep -q "TLS_REQCERT" /etc/ldap/ldap.conf)
 then
 	echo "TLS_REQCERT isn't present"
-    echo -e "TLS_REQCERT\tnever" >> /etc/ldap/ldap.conf
+	echo -e "TLS_REQCERT\tnever" >> /etc/ldap/ldap.conf
 fi
 
-#Téléchargement et extraction des sources de GLPI
+# Загрузка и извлечение исходников GLPI
 if [ "$(ls ${FOLDER_WEB}${FOLDER_GLPI})" ];
 then
-	echo "GLPI is already installed"
+	echo "GLPI уже установлен."
 else
 	SRC_GLPI=$(curl -s https://api.github.com/repos/glpi-project/glpi/releases/tags/${VERSION_GLPI} | jq .assets[0].browser_download_url | tr -d \")
 	TAR_GLPI=$(basename ${SRC_GLPI})
@@ -37,20 +37,20 @@ else
 	chown -R www-data:www-data ${FOLDER_WEB}${FOLDER_GLPI}
 fi
 
-#Adapt the Apache server according to the version of GLPI installed
-## Extract local version installed
+# Адаптация сервера Apache в соответствии с установленной версией GLPI.
+## Извлечь установленную локальную версию
 LOCAL_GLPI_VERSION=$(ls ${FOLDER_WEB}/${FOLDER_GLPI}/version)
-## Extract major version number
+## Извлечь номер основной версии
 LOCAL_GLPI_MAJOR_VERSION=$(echo $LOCAL_GLPI_VERSION | cut -d. -f1)
-## Remove dots from version string
+## Удалить точки из строки версии
 LOCAL_GLPI_VERSION_NUM=${LOCAL_GLPI_VERSION//./}
 
-## Target value is GLPI 1.0.7
-TARGET_GLPI_VERSION="10.0.7"
+## Целевое значение — GLPI 1.0.14
+TARGET_GLPI_VERSION="10.0.14"
 TARGET_GLPI_VERSION_NUM=${TARGET_GLPI_VERSION//./}
 TARGET_GLPI_MAJOR_VERSION=$(echo $TARGET_GLPI_VERSION | cut -d. -f1)
 
-# Compare the numeric value of the version number to the target number
+# Сравнение числового значения номера версии с целевым номером
 if [[ $LOCAL_GLPI_VERSION_NUM -lt $TARGET_GLPI_VERSION_NUM || $LOCAL_GLPI_MAJOR_VERSION -lt $TARGET_GLPI_MAJOR_VERSION ]]; then
   echo -e "<VirtualHost *:80>\n\tDocumentRoot /var/www/html/glpi\n\n\t<Directory /var/www/html/glpi>\n\t\tAllowOverride All\n\t\tOrder Allow,Deny\n\t\tAllow from all\n\t</Directory>\n\n\tErrorLog /var/log/apache2/error-glpi.log\n\tLogLevel warn\n\tCustomLog /var/log/apache2/access-glpi.log combined\n</VirtualHost>" > /etc/apache2/sites-available/000-default.conf
 else
@@ -58,16 +58,16 @@ else
   echo -e "<VirtualHost *:80>\n\tDocumentRoot /var/www/html/glpi/public\n\n\t<Directory /var/www/html/glpi/public>\n\t\tRequire all granted\n\t\tRewriteEngine On\n\t\tRewriteCond %{REQUEST_FILENAME} !-f\n\t\n\t\tRewriteRule ^(.*)$ index.php [QSA,L]\n\t</Directory>\n\n\tErrorLog /var/log/apache2/error-glpi.log\n\tLogLevel warn\n\tCustomLog /var/log/apache2/access-glpi.log combined\n</VirtualHost>" > /etc/apache2/sites-available/000-default.conf
 fi
 
-#Add scheduled task by cron and enable
+# Добавление запланировой задачи по cron
 echo "*/2 * * * * www-data /usr/bin/php /var/www/html/glpi/front/cron.php &>/dev/null" > /etc/cron.d/glpi
-#Start cron service
+# Запуск службы cron
 service cron start
 
-#Activation du module rewrite d'apache
+# Активация модуля rewrite в Apache
 a2enmod rewrite && service apache2 restart && service apache2 stop
 
-#Fix to really stop apache
+# Фикс, реально останавливающий Apache
 pkill -9 apache
 
-#Lancement du service apache au premier plan
+# Запуск службы Apache в FOREGROUND
 /usr/sbin/apache2ctl -D FOREGROUND
